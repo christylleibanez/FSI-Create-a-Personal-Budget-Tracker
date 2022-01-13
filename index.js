509da9725eb2055106c0d1a7a99f75fa0ee41721
“christylleibanez” <christylle.ibanez@gmail.com>
2022-01-12 19:37:02 -0500

    changed js

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
-let weeklyExpenses = 0
-let monthlyExpenses = 0
-let annualExpenses = 0
-
 for(let i = 0; i < weeklyExpenseQuestions.length; i++ ){
     let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
     weeklyExpenses = weeklyExpenses + answer
